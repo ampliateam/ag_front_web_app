88f617610b1c21f15bf8a08ddb4a30eec2c4c664
Jessica Alarcon <jessica.alarcon@konecta.com.py>
2024-07-10 21:14:02 -0400

    #21 Agrega inicializacion de registro

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,18 @@
-module.exports = {
+export default {
   env: {
-    node: true,
+    browser: true,
+    es2021: true,
   },
   extends: [
     'eslint:recommended',
-    "plugin:vue/vue3-recommended",
-    "prettier"
+    'plugin:@typescript-eslint/recommended',
   ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
   rules: {
-    // override/add rules settings here, such as:
-    // 'vue/no-unused-vars': 'error'
-  }
-}
+    // tus reglas aquí
+  },
+};
--- src/App.vue
+++ src/App.vue
@@ -7,8 +7,8 @@ import { onBeforeMount } from 'vue';
 import useUserStore from '@/store/use-user-store.store';
 import router from './router';
 
-onBeforeMount(() => {
-  const userEmail = useUserStore().getEmail;
-  if (!userEmail) router.push('/inicio-sesion');
-});
+// onBeforeMount(() => {
+//   const userEmail = useUserStore().getEmail;
+//   if (!userEmail) router.push('/inicio-sesion');
+// });
 </script>
